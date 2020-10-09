3d56b34b39c4eb174ecd96aaad3f24f3119a7cec
Alex <46821444+manlyalex@users.noreply.github.com>
2020-10-09 10:55:04 +0300

    Update SumOfAllNumbersThirteen.java

--- 02-jc1-les02-apps/src/by/htp/les02/main/SumOfAllNumbersThirteen.java
+++ 02-jc1-les02-apps/src/by/htp/les02/main/SumOfAllNumbersThirteen.java
@@ -5,18 +5,23 @@ import java.util.Scanner;
 public class SumOfAllNumbersThirteen {
 
 	public static void main(String[] args) {
-		int rezult = 0;
 		Scanner number = new Scanner(System.in);
-		int myNumber = number.nextInt();
-		while (myNumber <= 0) {
-			while (!number.hasNextInt()) {
-				System.out.print("Enter The integer number\n ");
+		int rezult = 0;
+		boolean Check;
+		int myNumber;
+		while (true) {
+			System.out.print("Enter The integer number\n ");
+			Check = number.hasNextInt();
+			if (!Check) {
 				number.next();
+				continue;
 			}
-			System.out.print("Enter The number > 0\n ");
 			myNumber = number.nextInt();
+			if (myNumber <= 0) {
+				continue;
+			}
+			break;
 		}
-
 		for (int i = 1; i <= myNumber; i++) {
 			rezult = i + rezult;
 		}
