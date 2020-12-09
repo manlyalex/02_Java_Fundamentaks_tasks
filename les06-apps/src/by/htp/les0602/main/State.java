7fa43e5f738ff306db53b840f9f3345dd27f4cc8
Alex <46821444+manlyalex@users.noreply.github.com>
2020-12-09 19:49:06 +0300

    Update State.java

--- les06-apps/src/by/htp/les0602/main/State.java
+++ les06-apps/src/by/htp/les0602/main/State.java
@@ -27,7 +27,21 @@ public class State {
     public void setDistricts(List<District> districts) {
         this.districts = districts;
     }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        State state = (State) o;
+        return Objects.equals(stateCenter, state.stateCenter) &&
+                Objects.equals(districts, state.districts);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(stateCenter, districts);
+    }
+    
     @Override
     public String toString() {
         return String.format("%s %s", getStateCenter(), getDistricts());
