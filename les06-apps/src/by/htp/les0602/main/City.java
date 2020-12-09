dc8e88286ad3a53a96a2454588eda7b5906325d3
Alex <46821444+manlyalex@users.noreply.github.com>
2020-12-09 19:47:15 +0300

    Update City.java

--- les06-apps/src/by/htp/les0602/main/City.java
+++ les06-apps/src/by/htp/les0602/main/City.java
@@ -13,7 +13,19 @@ public class City {
     public void setName(String name) {
         this.name = name;
     }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return Objects.equals(name, city.name);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
     @Override
     public String toString() {
         return String.format("%s", getName());
