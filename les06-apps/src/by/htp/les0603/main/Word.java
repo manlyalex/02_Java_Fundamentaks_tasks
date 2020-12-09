ae865e6ff45c03577748ffa0e28ad92ebddc87e9
Alex <46821444+manlyalex@users.noreply.github.com>
2020-12-09 19:51:42 +0300

    Update Word.java

--- les06-apps/src/by/htp/les0603/main/Word.java
+++ les06-apps/src/by/htp/les0603/main/Word.java
@@ -14,7 +14,20 @@ public class Word {
     public void setWord(String word) {
         this.word = word;
     }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Word word1 = (Word) o;
+        return Objects.equals(word, word1.word);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(word);
+    }
+    
     @Override
     public String toString() {
         return String.format("%s", getWord());
