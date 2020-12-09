b532eea8a402e614d762caf488eab500dc0ce2a2
Alex <46821444+manlyalex@users.noreply.github.com>
2020-12-09 19:50:44 +0300

    Update Sentence.java

--- les06-apps/src/by/htp/les0603/main/Sentence.java
+++ les06-apps/src/by/htp/les0603/main/Sentence.java
@@ -16,7 +16,20 @@ public class Sentence {
     public void setWords(List<Word> words) {
         this.words = words;
     }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sentence sentence = (Sentence) o;
+        return Objects.equals(words, sentence.words);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(words);
+    }
+    
     @Override
     public String toString() {
         return String.format("%s", getWords());
